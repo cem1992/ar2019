dc7b892f06aec936324ff9112b17ee1f23bdf389
floybox <floyd@heitzer.nl>
2019-02-11 12:45:47 +0100

    Revert "Scene 2"
    This reverts commit 5ab27940f9b498b7d9393b7c902dcddfcc7df328.

---
+++ Assets/collision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public GameObject earth;
+    public GameObject meteor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector3 earthPosistion = earth.transform.position;
+      Vector3 meteorPosistion = meteor.transform.position;
+      float distance = Vector3.Distance(earthPosistion, meteorPosistion);
+      
+      float earthLS_x = earth.transform.lossyScale.x;
+      float meteorLS_x = meteor.transform.lossyScale.x;
+
+      float radius_x = (earthLS_x + meteorLS_x) / 2;
+
+      if( distance < radius_x) {
+        Explode();
+      }
+
+    }
+    void Explode () {
+       print ("Impact");
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        //Destroy(gameObject, exp.duration);
+    }
+}
