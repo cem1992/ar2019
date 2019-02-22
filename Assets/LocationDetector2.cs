a7e535fa8ff1632272b06c143bd79886add3462f
floybox <floyd@heitzer.nl>
2019-02-22 13:30:49 +0100

    scene 2

--- Assets/LocationDetector2.cs
+++ Assets/LocationDetector2.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class LocationDetector2 : MonoBehaviour
 {
+	private GameObject Shuttle;
 	private GameObject Earth;
+	private GameObject LD;
     void Start()
     {
 		Earth = GameObject.Find("Earth");
+		Shuttle = GameObject.Find("Spaceshuttle");
+		LD = GameObject.Find("LD");
     }
 
  
     void Update()
     {
-
-
 	}
 
 	void OnGUI()
 	{
-		Matrix4x4 mat = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
-		mat = mat * T(Earth.transform.position.x, Earth.transform.position.y, Earth.transform.position.z);
-		Vector3 p = mat.GetColumn(3);
+		
+		Matrix4x4 mat = Matrix4x4.TRS(Shuttle.transform.position, Shuttle.transform.rotation, Shuttle.transform.lossyScale);
+		Matrix4x4 Earthmat = Matrix4x4.TRS(Earth.transform.position, Earth.transform.rotation, Earth.transform.lossyScale);
+		Matrix4x4 mat2 = Earthmat.inverse * mat;
+		Vector3 LP = mat2.MultiplyPoint(LD.transform.localPosition);
+		float q = Mathf.Sqrt(LP.x*LP.x + LP.z*LP.z);
 
    		GUI.color = Color.red;
-   		GUI.Label(new Rect(10, 10, 500, 100), "Local position: " + mat);
+   		GUI.Label(new Rect(10, 10, 500, 100), "Local position: " + LP.x + " " + LP.y + " "+ LP.z);
+		if(0 < LP.z && LP.y < 0.3 && q < 0.5) 
+		{  		GUI.color = Color.red;
+   		GUI.Label(new Rect(10, 50, 500, 100), "North");
+		}
+		if(0 > LP.z && LP.y < 0.3 && q < 0.5) 
+		{  		GUI.color = Color.red;
+   		GUI.Label(new Rect(10, 50, 500, 100), "South");
+		}
 	}	
 
 	public static Matrix4x4 T (float x, float y, float z)
